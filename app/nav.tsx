7a387933fc264105aa13ce4a05e199c45c39c53f
Lee Seul <love.and.seul@gmail.com>
2025-11-09 15:58:32 +0900

    fix: use absolute path to ensure correct navigation

--- app/nav.tsx
+++ app/nav.tsx
@@ -5,7 +5,7 @@ import ThemeChanger from "@/components/theme-changer";
 export default function Nav() {
   return (
     <div className="flex items-center gap-5">
-      <Link href="projectbox" className="btn-icon">
+      <Link href="/projectbox" className="btn-icon">
         <ArchiveIcon className="h-6 w-6" />
       </Link>
       <ThemeChanger />
