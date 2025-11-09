a54ef9937990a8993867982366cb22bb97b3f934
Lee Seul <love.and.seul@gmail.com>
2025-11-09 15:59:06 +0900

    design: show different icons based on project existence

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,35 @@
-import { BoxIcon, PlusSquareIcon } from "lucide-react";
+import { BoxIcon, PackageOpen, PlusSquareIcon } from "lucide-react";
 import Link from "next/link";
+import { use } from "react";
+import prisma from "@/lib/db";
 
 export default function Home() {
+  const projects = use(
+    prisma.project.findMany({
+      select: {
+        id: true,
+      },
+    }),
+  );
+  // const projects = []; // for debugging
+  const hasProjects = projects.length > 0;
   return (
     <div className="mx-auto flex h-full flex-col place-items-center justify-center gap-40 bg-amber-500x">
-      <Link href="/projectbox" className="bg-blue-400x p-10">
-        {/* TODO: when click, delay 0.5s & show openBox icon */}
-        <BoxIcon className="cursor-pointer" size={60} />
-      </Link>
+      {hasProjects ? (
+        <Link href="/projectbox" className="bg-blue-400x p-10">
+          <BoxIcon color="#e59a9a" className="cursor-pointer" size={60} />
+        </Link>
+      ) : (
+        <Link href="/projectbox/new" className="p-10">
+          <PackageOpen
+            color="#e59a9a"
+            className="cursor-pointer" //TODO: change border color
+            size={60}
+          />
+        </Link>
+      )}
       <Link href="/projectbox/new" className="p-10">
-        <PlusSquareIcon className="cursor-pointer" size={60} />
+        <PlusSquareIcon color="#e59a9a" className="cursor-pointer" size={60} />
       </Link>
     </div>
   );
