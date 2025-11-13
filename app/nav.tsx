e4eb227b9ee87a25243bca53ed31a057956b0b6c
Lee Seul <love.and.seul@gmail.com>
2025-11-13 18:56:00 +0900

    feat: make folder icon in nav change based on address

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,12 +1,12 @@
-import { FolderClosed } from "lucide-react";
 import Link from "next/link";
 import ThemeChanger from "@/components/theme-changer";
+import FolderIcon from "./folder-icon";
 
 export default function Nav() {
   return (
     <div className="flex items-center gap-5">
       <Link href="/projectbox" className="btn-icon">
-        <FolderClosed className="h-6 w-6" />
+        <FolderIcon />
       </Link>
       <ThemeChanger />
     </div>
