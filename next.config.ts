63421c9660c66b19b7ad43cbbab8bd554fc7ad7e
Lee Seul <love.and.seul@gmail.com>
2025-12-11 18:05:40 +0900

    chore: update dependencies, Next.js config

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["pdf-parse", "canvas", "pdf-img-convert"],
   experimental: {
     serverActions: {
       bodySizeLimit: "100mb", // image size setting : <= 10MB
     },
-    serverComponentsExternalPackages: ["pdf-parse"],
   },
 };
 
